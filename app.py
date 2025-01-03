5c1158a7ebcc7aab9de8160cf3c78754528ec930
ARTech <gialac120@gmail.com>
2025-01-03 05:23:15 +0000

    app

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import gradio as gr
 from fastai.vision.all import *
-import webbrowser as wb
-import requests
 
 path = Path('model.pkl')
 learn_inf = load_learner(path)
-url = 'https://www.youtube.com/watch?v=_HCqOqGwDfE'
-response = requests.get(url)
-if response.status_code == 200:
-    response = 'Accessed successfully.'
-else:
-    response = 'Failed to reach the URL.'
 
 def img(input_img):
     input_img = PILImage.create(input_img)
     rc, _, _ = learn_inf.predict(input_img)
     if rc == 'black':
         wb.open(url)
-    return rc, response
+    return rc
 
-app = gr.Interface(fn=img, inputs=gr.Image(type='filepath'), outputs='text')
+app = gr.Interface(
+    fn=img,
+    inputs=gr.Image(type='filepath'),
+    outputs='text')
+    
 app.launch()
