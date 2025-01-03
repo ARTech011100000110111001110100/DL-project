019a368aedf41014633258cba8bc5364c554dce0
ARTech <gialac120@gmail.com>
2025-01-03 05:30:22 +0000

    app

---
+++ app.py
@@ -0,0 +1,19 @@
+import gradio as gr
+from fastai.vision.all import *
+
+path = Path('model.pkl')
+learn_inf = load_learner(path)
+
+def img(input_img):
+    input_img = PILImage.create(input_img)
+    rc, _, _ = learn_inf.predict(input_img)
+    if rc == 'black':
+        wb.open(url)
+    return rc
+
+app = gr.Interface(
+    fn=img,
+    inputs=gr.Image(type='filepath'),
+    outputs='text')
+    
+app.launch()
